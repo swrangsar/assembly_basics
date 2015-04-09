9b538e661a4a5d36d45c00bff725d9e822b88055
swrangsar basumatary <swrangsar@swrangsar.com>
2015-04-09 19:47:34 +0530

    add two bytes

--- hello.asm
+++ hello.asm
@@ -11,11 +11,29 @@ _start:
     mov     rdx,len
     syscall
 
+    
+    mov rax, [n1] 
+    mov rbx, [n2]
+    add rax, rbx
+    mov [res], rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, res
+    mov [res+1], byte 0xa
+    mov rdx, 2
+    syscall
+
     mov     rax,60
     mov     rdi,0
     syscall
 
 section     .data
 
-msg db  `Hello, world!\n`,0
+msg db 'Hello, world!',10,0
 len equ $ - msg
+
+n1  db  77 
+n2  db  13
+
+section     .bss
+res resb    1
