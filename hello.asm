213f0a020999330b6d1c54e8fc6d23e3ee2d1aff
swrangsar basumatary <swrangsar@swrangsar.com>
2015-04-09 18:00:44 +0530

    first commit

---
+++ hello.asm
@@ -0,0 +1,21 @@
+; nasm -f elf64 hello.asm
+; ld -s -o hello hello.o
+
+section     .text
+global      _start
+
+_start:
+    mov     rax,1
+    mov     rdi,1
+    mov     rsi,msg
+    mov     rdx,len
+    syscall
+
+    mov     rax,60
+    mov     rdi,0
+    syscall
+
+section     .data
+
+msg db 'Hello, world!',10,0
+len equ $ - msg
